476dc0cbf2bd3e913af067669f9af28699a8965d
karen <quemojojojo@gmail.com>
2025-02-23 22:10:35 -0500

    config: Tailwind config with typography and color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,57 @@
-import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+import { type Config } from "tailwindcss";
 
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+const config: Config = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "landing-image": "url('/img/bg-digital-money.webp')",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        transparent: "transparent",
+        current: "currentColor",
+        black: "#000",
+        white: "#fff",
+        primary: {
+          DEFAULT: "#C1FD35",
+        },
+        secondary: {
+          DEFAULT: "#3A393E",
+        },
+        terciary: {
+          DEFAULT: "#EEEAEA",
+          light: "#eeeaea",
+        },
+        "black-primary": {
+          DEFAULT: "#201F22",
+        },
+        error: {
+          DEFAULT: "#EE3838",
+        },
+        gray: {
+          DEFAULT: "#CECECE",
+        },
+      },
+      fontFamily: {
+        sans: ["Open Sans", ...fontFamily.sans],
+      },
+      fontSize: {
+        base: ["1rem", "1.5rem"], // 16px
+        xs: ["0.75rem", "1.25rem"], // 12px
+        sm: ["0.875rem", "1.25rem"], // 14px
+        md: ["1.125rem", "1.75rem"], // 18px
+        lg: ["1.25rem", "1.75rem"], // 20px
+        xl: ["1.5rem", "2rem"], // 24px
+        "2xl": ["2rem", "2.5rem"], // 32px
+        "3xl": ["2.5rem", "3rem"], // 40px
+        "4xl": ["3rem", "3.5rem"], // 48px
+        "5xl": ["4rem", "4.5rem"], // 64px
+        "6xl": ["4.5rem", "5rem"], // 72px
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
